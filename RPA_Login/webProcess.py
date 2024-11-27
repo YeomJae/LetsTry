f669ac6649509cee9e8965d1d934b943c956f25b
YeomJae <baeaek@gmail.com>
2024-11-27 18:23:21 +0900

    download

--- RPA_Login/webProcess.py
+++ RPA_Login/webProcess.py
@@ -3,43 +3,71 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import os
+import util as ut
 
-id = "byj"
-pw = "Alsk123!"
+config = ut.load_config()
+current_dir = ut.exedir('py')
+download_dir = os.path.join(current_dir, "download")
+if not os.path.exists(download_dir):
+    os.makedirs(download_dir)
 
 chrome_options = Options()
 # chrome_options.add_argument("headless")
 chrome_options.add_experimental_option("detach", True)
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_argument("--disable-blink-features=AutomationControlled")  # 자동화 탐지 방지
+chrome_options.add_argument("--disable-features=IsolateOrigins,site-per-process")  # 일부 보안 기능 비활성화
+chrome_options.add_experimental_option("prefs", {
+"download.default_directory": download_dir,  # 다운로드 폴더 지정
+"download.prompt_for_download": False,  # 다운로드 시 사용자에게 묻지 않음
+"download.directory_upgrade": True,
+"safebrowsing.enabled": True,
+"credentials_enable_service": False,
+"profile.password_manager_enabled": False})
 
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://iacf.sejong.ac.kr/main_0001_08.act")
 driver.maximize_window()
+driver.find_element(By.CSS_SELECTOR, config["iacf"]["ID_CSS"]).send_keys(config["iacf"]["ID"])
+driver.find_element(By.CSS_SELECTOR, config["iacf"]["PW_CSS"]).send_keys(config["iacf"]["PW"])
+driver.find_element(By.CSS_SELECTOR, config["iacf"]["LOGIN_BT_CSS"]).click()
+sleep(5)
 
-user_id = driver.find_element(By.CSS_SELECTOR, "#USER_NM")
-user_id.send_keys("751370")
-rerppw = driver.find_element(By.CSS_SELECTOR, "#USER_PW")
-rerppw.send_keys(pw)
-rlogin_btn = driver.find_element(By.CSS_SELECTOR, "#btn_login").click()
-sleep(10)
-
-mainw = driver.window_handles
-for i in mainw:
-    if i != mainw[0] :
+main_window = driver.window_handles
+for i in main_window:
+    if i != main_window[0]:
         driver.switch_to.window(i)
         driver.close()
+sleep(3)
 
+driver.switch_to.window(main_window[0])
+driver.find_element(By.CSS_SELECTOR, "#menu_id_3").click()
+iframes = driver.find_elements(By.TAG_NAME, 'iframe')
+# for i, iframe in enumerate(iframes):
+#    print(f"iframe {i}: {iframe.get_attribute('id')}, {iframe.get_attribute('name')}, {iframe.get_attribute('src')}")
+
+driver.switch_to.frame(iframes[1])
+driver.find_element(By.CSS_SELECTOR, "#START_DATE").clear()
+driver.find_element(By.CSS_SELECTOR, "#START_DATE").send_keys("20240501")
+driver.find_element(By.CSS_SELECTOR, "#btnSearch").click()
+sleep(10)
+driver.find_element(By.CSS_SELECTOR, "#gridDonw > img").click()
+sleep(3)
+
+driver.switch_to.default_content()
 driver.switch_to.window(driver.window_handles[0])
 driver.execute_script('window.open("https://mail.sejong.ac.kr");')
 
 driver.switch_to.window(driver.window_handles[1])
-mail_id = driver.find_element(By.CSS_SELECTOR, "#mailid")
-mail_id.send_keys(id)
-mqw = driver.find_element(By.CSS_SELECTOR, "#password")
-mqw.send_keys(pw)
-mlogin_btn = driver.find_element(By.CSS_SELECTOR, "#loginForm > div.login.skin1 > section > div > div.login_content > div.login_form.non_findId > div.input_area > ul > li.btn_line > button")
-mlogin_btn.click()
-mail_total = driver.find_element(By.CSS_SELECTOR, "#mnu_TOTAL")
-mail_total.click()
+driver.find_element(By.CSS_SELECTOR, config["mail"]["ID_CSS"]).send_keys(config["login"]["ID"])
+driver.find_element(By.CSS_SELECTOR, config["mail"]["PW_CSS"]).send_keys(config["login"]["PW"])
+driver.find_element(By.CSS_SELECTOR, config["mail"]["LOGIN_BT_CSS"]).click()
+driver.find_element(By.CSS_SELECTOR, config["mail"]["RECEIVE_MAIL_CSS"]).click()
 
-driver.switch_to.window(driver.window_handles[1])
+driver.switch_to.window(driver.window_handles[0])
 driver.execute_script('window.open("http://sjgw.sejong.ac.kr/");')
+
+# driver.find_element(By.CSS_SELECTOR, config["gw"]["ID_CSS"]).send_keys(config["login"]["ID"])
+# driver.find_element(By.CSS_SELECTOR, config["gw"]["PW_CSS"]).send_keys(config["login"]["PW"])
+# driver.find_element(By.CSS_SELECTOR, config["gw"]["LOGIN_BT_CSS"]).click()
